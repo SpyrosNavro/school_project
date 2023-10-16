b679d9eb678960866f55c44e520885bc1f86a93a
SpyrosNavro <spyrosnavro@gmail.com>
2023-10-16 18:24:37 +0300

    update graph.c

--- header_files/graph.h
+++ header_files/graph.h
@@ -1,3 +1,30 @@
-#header
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#hellooo
+#define N 6
+
+struct Graph {
+    struct Node* nodes[N];
+};
+
+struct Node {
+    int x, y;
+    int dest;
+    struct Node* next;
+};
+
+struct Edge {
+    int src, dest, weight;
+};
+
+struct Graph* createGraph (struct Edge edges[], int n) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+
+    srand(time(0));
+
+    for (int i = 0; i < n; i++) {
+        graph->nodes[i]->x = rand();
+        graph->nodes[i]->y = rand();
+    }
+}
