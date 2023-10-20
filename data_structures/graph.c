c7b7c2472fd309acb7e62a64a9bb7b406c51c167
SpyrosNavro <spyrosnavro@gmail.com>
2023-10-20 13:00:04 +0300

    added typedef and small changes

--- data_structures/graph.c
+++ data_structures/graph.c
@@ -4,28 +4,31 @@
 
 #define nnodes 6
 
-struct Graph {
-    struct Node* nodes[nnodes];
+typedef struct graph {
+    Node* nodes[nnodes];
     int dim;
-};
+} Graph;
 
 // structure of nodes 
-struct Node {
+typedef struct node {
     int* coord;  // coordinates of n-dimentional point
     int id;    // id of point 
     // struct Node* next;  not useful for now
-};
+} Node;
 
 // structure of edge
-struct Edge {
+typedef struct edge {
     int src, dest, weight;
-};
+} Edge;
 
 
-void import_data(const char *file_name)
+int** import_data(const char *file_name, int row, int column)
 {
     FILE* pointer;
     
+    // atoi: char => int
+    // fgetc: obtain input single character at a time. returns the ASCII code of the character read. it moves to the next character by itself
+
     pointer = fopen(file_name, "r");
     if (pointer == NULL) 
     {
@@ -42,18 +45,18 @@ void import_data(const char *file_name)
 }
 
 
-struct Graph* createGraph (int nedges, const char *file_name) 
+struct Graph* createGraph (int nedges, const char *file_name, int row, int column) 
 {
-    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
-
-    // atoi: char => int
-    // fgetc: obtain input single character at a time. returns the ASCII code of the character read. it moves to the next character by itself
+    Graph* graph = (struct Graph*)malloc(sizeof(Graph));
+    int** data = import_data(file_name, row, column);
 
     // initialise nodes 
-    for (int i = 0; i < nnodes; i++) {
-        graph->nodes[i]->x = rand();
-        graph->nodes[i]->y = rand();
-        graph->nodes[i]->id = i++;
+    for (int row = 0; row < nnodes; row++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            *(graph->nodes[row]->coord + j) = data[row][column];
+        }
     }
 
     // add edges to the directed graph one by one
@@ -61,7 +64,7 @@ struct Graph* createGraph (int nedges, const char *file_name)
     {
         for (int j = 0; j < nedges; j++)
         {
-            struct Edge* edge = (struct Edge*)malloc(sizeof(struct Edge));
+            Edge* edge = (struct Edge*)malloc(sizeof(Edge));
             
             edge->src = id;
             do 
