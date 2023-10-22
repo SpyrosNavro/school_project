c9b23730162d9b4b542ebce2ad47999c97c74be1
Georgina12 <sdi1800099@di.uoa.gr>
2023-10-22 21:25:22 +0300

    import_data

--- data_structures/graph.c
+++ data_structures/graph.c
@@ -22,25 +22,8 @@ struct Edge {
 };
 
 
-void import_data(const char *file_name)
-{
-    FILE* pointer;
-    
-    pointer = fopen(file_name, "r");
-    if (pointer == NULL) 
-    {
-        print("no file was found.");
-        return 0;
-    }
 
-    while ( !(feof(pointer)) )
-    {
-        // insesrt char
-        // transform string into int
-        // save into node->coord
-    }
-}
-
+/*
 
 struct Graph* createGraph (int nedges, const char *file_name) 
 {
@@ -51,8 +34,8 @@ struct Graph* createGraph (int nedges, const char *file_name)
 
     // initialise nodes 
     for (int i = 0; i < nnodes; i++) {
-        graph->nodes[i]->x = rand();
-        graph->nodes[i]->y = rand();
+    //    graph->nodes[i]->x = rand();
+    //    graph->nodes[i]->y = rand();
         graph->nodes[i]->id = i++;
     }
 
@@ -73,5 +56,79 @@ struct Graph* createGraph (int nedges, const char *file_name)
 
     return graph;
 }
+*/
 
+
+int** import_data(const char *file_name)
+{
+    FILE* pointer;
+     
+    pointer = fopen(file_name, "r");  // reading the file 
+    if (pointer == NULL) 
+    {
+        printf("no file was found.");
+        //return 1;
+    }
+     
+    int** vector = NULL; // dynamilly allocated array (vector of vectors )
+    int vrows=9759;
+    int vcol=3;
+     
+    
+    vector = (int**) malloc(vrows * sizeof(int * )); // 2dimensional 
+    if (vector == NULL){
+        printf("error allocating memory for the vector");
+        fclose(pointer);
+        //return 1;
+    }
+    
+    for (int i =0; i< vrows; i++){
+        vector[i] = (int *)malloc(vcol * sizeof(int));  // 2D array , allocate memory for 3 elements of every row 
+        
+        if (vector[i] == NULL){
+            printf("memory allocation error");
+            free(vector);
+            fclose(pointer);
+             
+        }
+
+    }
+
+    
+
+    // save the elements of the file inside the vector (every row has three columns - its coordinates )
+    for (int i= 0; i < vrows; i++){
+        int j=0;
+        int indicator=0;
+        
+            if (fscanf(pointer, "%d %d %d %d", &indicator, &vector[i][j], &vector[i][j+1], &vector[i][j+2]) != vcol+1) {
+                printf("Error reading the coordinates");
+            }
+
+        
+    }
+
+    // printing coordinates  
+    /*
+    for (int i =0; i< 3; i++){
+        printf("row %d:", i+1);
+        for(int j=0; j < vcol; j++) {
+            printf("%d \n",vector[i][j]);
+             
+        }
+    }
+    */
+
+    // freeing the memory of the vector 
+    for (int i=0; i< vrows; i++){
+        free(vector[i]);
+
+    }
+    free(vector);
+    fclose(pointer);
+   
+    
+    
+   return vector; 
+}
 
