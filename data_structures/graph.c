63678a6e272d78803862d03239a4fbdb0fb9ed55
SpyrosNavro <spyrosnavro@gmail.com>
2023-10-23 19:56:16 +0300

    edited create

--- data_structures/graph.c
+++ data_structures/graph.c
@@ -22,89 +22,12 @@ struct edge {
 
 
 
-Graph createGraph (int nedges, const char *file_name, int row, int column) 
-{
-    Graph graph = malloc(sizeof(*graph));
-    //int** data = import_data(file_name, row, column);
-    
-    graph->dim = column;
-    graph->nnodes = row;
-    graph->neighbors = nedges;
-
-    // initialise nodes 
-    for (int id = 0; id < row; id++)
-    {
-        for (int j = 0; j < column; j++)
-        {
-            graph->nodes[id] = malloc(sizeof( *(graph->nodes[id]) ));   // allocate node
-            graph->nodes[id]->coord[j] = data[id][j];                   // put data in node
-            
-            //*(graph->nodes[i]->coord + j) = data[i][j];
-        }
-
-        // add directed edges to each node
-        for (int j = 0; j < nedges; j++)
-        {
-            Edge edge = malloc(sizeof(*edge));
-            graph->nodes[id]->edges[j] = edge;
-            edge->src = id;
-
-            do 
-            {
-                edge->dest = rand();
-                edge->distance = compute_distance(graph->nodes[id], graph->nodes[edge->dest], graph->dim);
-            } while ( (edge->dest == id) && (edge->dest < 0) && (edge->dest >= graph->nnodes) );
-        }
-    }
-
-    // add directed edges to each node
-    // for (int id = 0; id < row; id++)
-    // {
-    //     for (int j = 0; j < nedges; j++)
-    //     {
-    //         Edge edge = malloc(sizeof(*edge));
-    //         graph->nodes[id]->edges[j] = edge;
-    //         edge->src = id;
-
-    //         do 
-    //         {
-    //             edge->dest = rand();
-    //         } while ( (edge->dest == id) && (edge->dest < 0) && (edge->dest >= graph->nnodes) );
-    //     }
-    // }
-
-    return graph;
-}
-
-
-
-
-
-void deleteGraph(Graph graph)
-{   
-    for (int i = graph->nnodes - 1; i >= 0; i--)
-    {
-        for (int j = 0; j < graph->neighbors; j++)
-        {
-            free(graph->nodes[i]->edges[j]);
-        }
-        free(graph->nodes[i]);
-    }
-
-    free(graph);
-    return 0;
-}
-
-
-
-
-
 int compute_distance (Node a, Node b, int dim)
 {
     int sum = 0;
     for (int i = 0; i < dim; i++) 
     {
-        sum = sum + pow(a->coord[i] + b->coord[i], 2);
+        sum = sum + pow(a->coord[i] - b->coord[i], 2);
     }
     sum = sqrt(sum);
     return sum;
@@ -181,3 +104,62 @@ int** import_data(const char *file_name, int vrows)
    return vector; 
 }
 
+
+
+
+
+Graph createGraph (int nedges, const char *file_name, int row, int column) 
+{
+    Graph graph = malloc(sizeof(*graph));
+    int** data = import_data(file_name, row);
+    
+    graph->dim = column;
+    graph->nnodes = row;
+    graph->neighbors = nedges;
+
+    // initialise nodes 
+    for (int id = 0; id < row; id++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            graph->nodes[id] = malloc(sizeof( *(graph->nodes[id]) ));   // allocate node
+            graph->nodes[id]->coord[j] = data[id][j];                   // put data in node
+        }
+
+        // add directed edges to each node
+        for (int j = 0; j < nedges; j++)
+        {
+            Edge edge = malloc(sizeof(*edge));   // allocate edge
+            edge->src = id;
+
+            do 
+            {
+                edge->dest = rand();
+                edge->distance = compute_distance(graph->nodes[id], graph->nodes[edge->dest], graph->dim);
+            } while ( (edge->dest == id) && (edge->dest < 0) && (edge->dest >= graph->nnodes) );
+
+            graph->nodes[id]->edges[j] = edge;
+        }
+    }
+    
+    return graph;
+}
+
+
+
+
+
+void deleteGraph(Graph graph)
+{   
+    for (int i = graph->nnodes - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < graph->neighbors; j++)
+        {
+            free(graph->nodes[i]->edges[j]);
+        }
+        free(graph->nodes[i]);
+    }
+
+    free(graph);
+    return 0;
+}
