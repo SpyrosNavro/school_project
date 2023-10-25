6d5b2e0cd4dde97554fc66ce098525029bd2bf2e
Georgina12 <sdi1800099@di.uoa.gr>
2023-10-25 17:37:10 +0300

    merging

--- header_files/graph.h
+++ header_files/graph.h
@@ -10,6 +10,6 @@ typedef struct node* Node;
 typedef struct edge* Edge;
 
 int** import_data(const char *file_name, int vrows);
-int compute_distance (Node a, Node b, int dim);
+float compute_distance(Node a, Node b, int dim);
 Graph createGraph(int nedges, const char *file_name, int row, int column);
 int deleteGraph(Graph graph);
