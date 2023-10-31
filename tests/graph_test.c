0c7c9103f295699b041beb41f2befc52e68dba57
SpyrosNavro <spyrosnavro@gmail.com>
2023-10-31 23:10:44 +0200

    unit testing for delete

--- tests/graph_test.c
+++ tests/graph_test.c
@@ -5,55 +5,42 @@
 #include "graph.h"
 
 
-/*
+
 void test_import(void) 
 {
-    int** array = import_data(file_name, row, column);
+    int row = 9759;
+    int** array = import_data("5k.txt", row);
 
     TEST_ASSERT(array != NULL);
 }
-*/
+
 void test_create(void)
 {
     int nedges = 2;
-    Graph graph = createGraph(nedges, file_name, row, column);
+    int row = 9759;
+    int column = 3;
+
+    Graph graph = createGraph(nedges, "5k.txt", row, column);
     TEST_ASSERT(graph != NULL);
     deleteGraph(graph);
+}
+
+void test_delete(void)
+{
+    int nedges = 2;
+    int row = 9759;
+    int column = 3;
+    int result;
+
+    Graph graph = createGraph(nedges, "5k.txt", row, column);
+    TEST_ASSERT(graph != NULL);
+    result = deleteGraph(graph);
+    TEST_ASSERT(graph == 1);
 }
 
 TEST_GRAPH = {
     { "import_data", test_import },
     { "createGraph", test_create },
+    { "deleteGraph", test_delete },
     { NULL, NULL }
 };
-
-int main(){
-     
-    char *filename = "data_structures/5k.txt";
-    int vrows=9759;
-    int** vector = import_data(filename, vrows);
-/*
-    for (int i =0; i< 3; i++){
-        printf("row %d:", i+1);
-        for(int j=0; j < 3; j++) {
-            printf("%d \n",vector[i][j]);
-             
-        }
-    }
-*/    
-    if(vector !=NULL ){
-        printf("Succesfully created the vector ");
-
-        // delete vector 
-        for (int i=0; i< vrows; i++){
-            free(vector[i]);
-        }
-        free(vector);
-    }
-    else{
-        printf("Failed to create vector ");
-    }
-    
-
-    return 0;
-}
