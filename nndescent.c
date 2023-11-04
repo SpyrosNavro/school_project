ab116bcd218b9dbed85cec4584134eb47a57afa1
SpyrosNavro <spyrosnavro@gmail.com>
2023-11-04 21:19:58 +0200

    modified nndescent

--- nndescent.c
+++ nndescent.c
@@ -1,8 +1,7 @@
 #include "graph.h"
 #include "pqueue.h"
 
-typedef struct pqueue* PQ;
-int pqueue_insert(PQ queue, Node a, int distance);
+typedef struct PQueue* PQ;
 
 int main(void)
 {
@@ -13,10 +12,10 @@ int main(void)
     int col = 3;
     int flag = 0;
     int randomid;
-    int distance;
+    float distance;
     int* point;
     int* start;
-    PQ queue;
+    PQ queue[row];
     int neighbor, neighbor_of_neighbor, neighbor_of_reverse;
     int reverse, reverse_of_neighbor, reverse_of_reverse;
 
@@ -30,28 +29,35 @@ int main(void)
         point[i] = rand();
     }
 
+    // create Priority Queue for every node
+    for (int id = 0; id < row; id++)
+    {
+        queue[id] = createPQueue(row);
+    }
+
     do
     {
         flag = 1;
         
         for (int id = 0; id < row; id++)
         {
-            
+
 
             // NEIGHBORS
             for (int neighbors = 0; neighbors < graph->neighbors; neighbors++)
             {
                 // neighbor
                 neighbor = graph->nodes[id]->edges[neighbors]->dest;
-                pqueue_insert(queue, graph->nodes[neighbor], graph->nodes[id]->edges[neighbors]->distance);
-
+                insertPQueue(queue[id], graph->nodes[neighbor], graph->nodes[id]->edges[neighbors]->distance);
+                insertPQueue(queue[neighbor], graph->nodes[id], graph->nodes[id]->edges[neighbors]->distance);
 
                 // neighbor of neighbor
                 for (int i = 0; i < graph->neighbors; i++)
                 {
                     neighbor_of_neighbor = graph->nodes[neighbor]->edges[i]->dest;
                     distance = compute_distance(graph->nodes[id], graph->nodes[neighbor_of_neighbor], graph->dim);
-                    pqueue_insert(queue, graph->nodes[neighbor_of_neighbor], distance);
+                    insertPQueue(queue[id], graph->nodes[neighbor_of_neighbor], distance);
+                    insertPQueue(queue[neighbor_of_neighbor], graph->nodes[id], distance);
                 }
 
 
@@ -60,7 +66,8 @@ int main(void)
                 {
                     reverse_of_neighbor = graph->nodes[neighbor]->reverse[i]->src;
                     distance = compute_distance(graph->nodes[id], graph->nodes[reverse_of_neighbor], graph->dim);
-                    pqueue_insert(queue, graph->nodes[reverse_of_neighbor], distance);
+                    insertPQueue(queue[id], graph->nodes[reverse_of_neighbor], distance);
+                    insertPQueue(queue[reverse_of_neighbor], graph->nodes[id], distance);
                 }
 
 
@@ -73,7 +80,8 @@ int main(void)
             {
                 // reverse neighbor
                 reverse = graph->nodes[id]->reverse[rev]->src;
-                pqueue_insert(queue, graph->nodes[reverse], graph->nodes[id]->reverse[rev]->distance);
+                insertPQueue(queue[id], graph->nodes[reverse], graph->nodes[id]->reverse[rev]->distance);
+                insertPQueue(queue[reverse], graph->nodes[id], graph->nodes[id]->reverse[rev]->distance);
 
 
                 // neighbors of reverse neighbor
@@ -81,7 +89,8 @@ int main(void)
                 {
                     neighbor_of_reverse = graph->nodes[reverse]->edges[i]->dest;
                     distance = compute_distance(graph->nodes[id], graph->nodes[neighbor_of_reverse], graph->dim);
-                    pqueue_insert(queue, graph->nodes[neighbor_of_reverse], distance);
+                    insertPQueue(queue[id], graph->nodes[neighbor_of_reverse], distance);
+                    insertPQueue(queue[neighbor_of_reverse], graph->nodes[id], distance);
                 }
 
 
@@ -90,7 +99,8 @@ int main(void)
                 {
                     reverse_of_reverse = graph->nodes[reverse]->reverse[i]->src;
                     distance = compute_distance(graph->nodes[id], graph->nodes[reverse_of_reverse], graph->dim);
-                    pqueue_insert(queue, graph->nodes[reverse_of_reverse], distance);
+                    insertPQueue(queue[id], graph->nodes[reverse_of_reverse], distance);
+                    insertPQueue(queue[reverse_of_reverse], graph->nodes[id], distance);
                 }
 
 
